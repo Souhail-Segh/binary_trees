f19d92fceb10bbd2a0334292dcff7d78101c3184
root <root@DESKTOP-148OPAD.localdomain>
2023-11-02 22:22:18 +0200

    task 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
 
-size_t binary_tree_height2(const binary_tree_t *tree);
 /**
  * binary_tree_balance - Function that measures the balance of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the balance.
@@ -22,7 +21,7 @@ int binary_tree_balance(const binary_tree_t *tree)
  * @tree: A pointer to the root node of the tree to measure the height.
  * Return: 0 if tree is NULL.
  */
-size_t binary_tree_height2(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
