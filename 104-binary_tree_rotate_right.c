73e92a96d2da78c88b9922dd1d2fe7259ee11ed9
root <root@DESKTOP-148OPAD.localdomain>
2023-11-02 23:59:17 +0200

    task 104-binary_tree_rotate_right.c

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -7,15 +7,19 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *new_root;
+	binary_tree_t *new_root, *temp;
 
 	if (tree == NULL || tree->left == NULL)
 		return (tree);
 
 	new_root = tree->left;
 	new_root->parent->parent = new_root;
-	new_root->parent->left = new_root->right;
+	temp = new_root->right;
+	new_root->parent->left = temp;
+	if (temp)
+		temp->parent = tree;
 	new_root->right = tree;
+	tree->parent = new_root;
 	new_root->parent = NULL;
 
 	return (new_root);
