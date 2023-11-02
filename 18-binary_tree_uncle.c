5d26e857400ddb4b2fd4e620e25a625e58122c41
root <root@DESKTOP-148OPAD.localdomain>
2023-11-03 00:46:41 +0200

    task 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,10 +7,9 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node->parent->parent == NULL || node == NULL)
-	{
+	if (node == NULL || node->parent == NULL ||
+		node->parent->parent == NULL)
 		return (NULL);
-	}
 
 	if (node->parent->parent->left == node->parent)
 	{
